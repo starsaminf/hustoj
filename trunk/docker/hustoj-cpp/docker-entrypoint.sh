3f8f7417c37dff941373b6f91bd93fc06ffe1d33
Samuel Loza Ramirez <starsaminf@gmail.com>
2018-04-24 13:28:46 -0400

    Cambio minimo

--- trunk/docker/hustoj-cpp/docker-entrypoint.sh
+++ trunk/docker/hustoj-cpp/docker-entrypoint.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
-DIRECTORY="/home/judge/data/mysql"
+DIRECTORY="/home/judge/mysql"
 if [ ! -d $DIRECTORY ]; then
-	mv  /var/lib/mysql /data
+	mv  /var/lib/mysql /home/judge/mysql
 else
 	rm -R /var/lib/mysql
 fi
 ln -s $DIRECTORY /var/lib/mysql
 
-chmod 775 -R /data/data 
-chgrp -R www-data /data/data
-chmod 770 -R /data/upload 
-chgrp -R www-data /data/upload
-chmod 770 -R /data/judge.conf 
-chgrp -R www-data /data/judge.conf
-chmod 770 -R /data/db_info.inc.php
-chgrp -R www-data /data/db_info.inc.php
-
-#chown -R mysql:mysql /var/lib/mysql 
-chown -R mysql:mysql /data/mysql/
+chown -R mysql:mysql /home/judge/mysql
 
 service mysql start
 /usr/bin/judged
